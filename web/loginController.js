858ce56014dfccb574afc16717daf4155d85ab9e
yangguanghe <1649063797@qq.com>
2020-08-05 23:48:32 +0800

    post请求

--- web/loginController.js
+++ web/loginController.js
@@ -3,20 +3,24 @@ const url = require('url');
 
 const path = new Map();
 function login(request, response) {
-    const params = url.parse(request.url, true).query;
-    studentService.queryStudentBySNo(params.sNo,function(result) {
-        let res = '';
-        if (result === null || !result.length) {
+    request.on('data', function(data) {
+        const sNo = data.toString().split('&')[0].split('=')[1];
+        const password = data.toString().split('&')[1].split('=')[1];
+        console.log(sNo, password)
+        studentService.queryStudentBySNo(sNo,function(result) {
+            let res = '';
+            if (result === null || !result.length) {
 
-        } else {
-            if (result[0].pwd === params.password) {
-                res = 'ok';
             } else {
-                res = 'fail'
+                if (result[0].pwd === password) {
+                    res = 'ok';
+                } else {
+                    res = 'fail'
+                }
             }
-        }
-        response.write(res);
-        response.end();
+            response.write(res);
+            response.end();
+        });
     });
 }
 path.set('/login', login);
