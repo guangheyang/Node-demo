bff42db9589152dacb44cccd4e25a1dea2644571
yangguanghe <1649063797@qq.com>
2020-08-09 00:37:04 +0800

    修改加载文件异常

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const globalConfig = {};
 const conf = fs.readFileSync('./server.conf');
-const configArr = conf.toString().split('\n');
+const configArr = conf.toString().split('\r\n');
 
 for(let i = 0; i < configArr.length; i++) {
     globalConfig[configArr[i].split('=')[0]] = configArr[i].split('=')[1];
--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const loader = require('./loader');
 const app = new express();
 
 app.use(express.static(globalConfig['page_path']));
-// app.get('/api/*',function(request, response, next) {
-//     console.log('===');
-//     // response.writeHead(404);
-//     // response.write('<html><body>404NotFound</body></html>');
-//     // response.end();
-//     // response.redirect('/login.html');
-// });
+app.get('/api/*',function(request, response, next) {
+    console.log('===');
+    // response.writeHead(404);
+    // response.write('<html><body>404NotFound</body></html>');
+    // response.end();
+    response.redirect('/login.html');
+});
 loader.init(app);
 app.listen(globalConfig.port);
